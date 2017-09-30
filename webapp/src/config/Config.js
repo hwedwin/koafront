e004223de1b35d6bc62eb6cf32a80974f468219f
MANSOUL <xiaohu12347@gmail.com>
2017-09-30 12:37:10 +0800

    rebuild

--- webapp/src/config/Config.js
+++ webapp/src/config/Config.js
@@ -1,66 +1,69 @@
-var host = 'http://localhost:4000/';
+// var window.host = 'http://localwindow.host:4000/';
+if (typeof window.host === 'undefined') {
+	window.host = 'http://localhost:4000';
+}
 var Config = {
 	API: {
-		TOPADV_LIST: host+'api/topadv/list',
-		TOPSPE_LIST: host+'api/topspe/list',
+		TOPADV_LIST: window.host+'api/topadv/list',
+		TOPSPE_LIST: window.host+'api/topspe/list',
 
 		// 商品
-		DRINK_LIST: host+'api/drink/list',
-		DRINK_LISTREC: host+'api/drink/listrec',
-		DRINK_LISTHOT: host+'api/drink/listhot',
-		DRINK_ONE: host+'api/drink/one',
-		DRINK_CATE: host+'api/drink/listcate',//通过分类获取
-		DRINK_BRAND: host+'api/drink/listbrand',//通过品牌获取
-		DRINK_SEARCH: host+'api/drink/search',
+		DRINK_LIST: window.host+'api/drink/list',
+		DRINK_LISTREC: window.host+'api/drink/listrec',
+		DRINK_LISTHOT: window.host+'api/drink/listhot',
+		DRINK_ONE: window.host+'api/drink/one',
+		DRINK_CATE: window.host+'api/drink/listcate',//通过分类获取
+		DRINK_BRAND: window.host+'api/drink/listbrand',//通过品牌获取
+		DRINK_SEARCH: window.host+'api/drink/search',
 
 		// 收藏
-		FAV_ADD: host+'api/favorite/add',
-		FAV_DEL: host+'api/favorite/del',
-		FAV_COMBINE: host+'api/favorite/combine',
-		FAV_STATE: host+'api/favorite/state',
-		FAV_LIST: host+'api/favorite/list',
+		FAV_ADD: window.host+'api/favorite/add',
+		FAV_DEL: window.host+'api/favorite/del',
+		FAV_COMBINE: window.host+'api/favorite/combine',
+		FAV_STATE: window.host+'api/favorite/state',
+		FAV_LIST: window.host+'api/favorite/list',
 
 		// 用户
-		MEMBER_LOGIN: host+'api/member/login',
-		MEMBER_DATA: host+'api/member/data',
-		MEMBER_EDIT: host+'api/member/edit',
-		MEMBER_CUSTOMER: host+'api/member/customer',
+		MEMBER_LOGIN: window.host+'api/member/login',
+		MEMBER_DATA: window.host+'api/member/data',
+		MEMBER_EDIT: window.host+'api/member/edit',
+		MEMBER_CUSTOMER: window.host+'api/member/customer',
 
 		// 购物车
-		CART_ADD: host+'api/cart/add',
-		CART_GET: host+'api/cart/get',
-		CART_NUMS: host+'api/cart/nums',
-		CART_DEL: host+'api/cart/del',
-		CART_DEL_BID: host+'api/cart/delbid',
+		CART_ADD: window.host+'api/cart/add',
+		CART_GET: window.host+'api/cart/get',
+		CART_NUMS: window.host+'api/cart/nums',
+		CART_DEL: window.host+'api/cart/del',
+		CART_DEL_BID: window.host+'api/cart/delbid',
 
 		// 品牌
-		BRAND_LIST: host+'api/brand/list',
+		BRAND_LIST: window.host+'api/brand/list',
 
 		// 收货地址
-		CONSIGNEE_LIST: host + 'api/consignee/list',
-		CONSIGNEE_CREATE: host + 'api/consignee/create',
-		CONSIGNEE_ONE: host + 'api/consignee/one',
-		CONSIGNEE_MODIFY: host + 'api/consignee/modify',
+		CONSIGNEE_LIST: window.host + 'api/consignee/list',
+		CONSIGNEE_CREATE: window.host + 'api/consignee/create',
+		CONSIGNEE_ONE: window.host + 'api/consignee/one',
+		CONSIGNEE_MODIFY: window.host + 'api/consignee/modify',
 
 		// 订单
-		ORDER_CREATE: host + 'api/order/create',
-		ORDER_LIST: host + 'api/order/list',
-		ORDER_DETAIL: host + 'api/order/detail',
-		ORDER_COUNT: host + 'api/order/count',
-		ORDER_SIGN: host + 'api/order/sign',
+		ORDER_CREATE: window.host + 'api/order/create',
+		ORDER_LIST: window.host + 'api/order/list',
+		ORDER_DETAIL: window.host + 'api/order/detail',
+		ORDER_COUNT: window.host + 'api/order/count',
+		ORDER_SIGN: window.host + 'api/order/sign',
 
 		// 交易
-		TRANS_EXPENSE: host + 'api/trans/expense',
-		TRANS_SUM_ORDER: host + 'api/trans/sumorder',
+		TRANS_EXPENSE: window.host + 'api/trans/expense',
+		TRANS_SUM_ORDER: window.host + 'api/trans/sumorder',
 
 		// 文件上传
-		FILE_PORTRAIT: host + 'api/file/portrait',
+		FILE_PORTRAIT: window.host + 'api/file/portrait',
 
 		// 余额
-		BALANCE_GET: host + 'api/balance/get',
+		BALANCE_GET: window.host + 'api/balance/get',
 
 		// 心跳借口
-		BEAT: host + 'api/beat',
+		BEAT: window.host + 'api/beat',
 	}
 }
 
