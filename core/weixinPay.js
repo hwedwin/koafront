77fcb4a09d07eba4a9996db5bc3d44437107cb4f
MANSOUL <xiaohu12347@gmail.com>
2017-10-11 02:26:55 +0800

    bug fix

--- core/weixinPay.js
+++ core/weixinPay.js
@@ -5,6 +5,7 @@ var WXPay = require('weixin-pay');
 module.exports = {
 	createUniOrder: function(openid,orderId,totalPrice,payInfo,ip,url) {
 		ip = ip.match(/\d+.\d+.\d+.\d+/)[0];
+		console.log('ip:'+ip)
 		// 创建微信支付接口
 		var wxpay = WXPay({
 			appid: wxConfig.appid,
