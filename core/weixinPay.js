b911db1df40ff37ce712d217e4fe47331d2a03f1
MANSOUL <xiaohu12347@gmail.com>
2017-10-11 13:31:42 +0800

    bug fixed

--- core/weixinPay.js
+++ core/weixinPay.js
@@ -3,37 +3,6 @@ var wxConfig = require('../config/weixin');
 var util = require('./weixinUtil');
 var request = require('request');
 var md5 = require('MD5');
-/*module.exports = {
-	createUniOrder: function(openid,orderId,totalPrice,payInfo,ip,url) {
-		ip = ip.match(/\d+.\d+.\d+.\d+/)[0];
-		console.log('ip:'+ip)
-		// 创建微信支付接口
-		var wxpay = WXPay({
-			appid: wxConfig.appid,
-			mch_id: wxConfig.mch_id
-		});
-
-		return new Promise(function(resolve,reject){
-			wxpay.getBrandWCPayRequestParams({
-				openid: openid,
-				body: payInfo,
-			    detail: '公众号支付测试',
-				out_trade_no: orderId,//内部订单号
-				total_fee: totalPrice,
-				spbill_create_ip: ip,
-				notify_url: url
-			}, function(err, result){
-				if (err) {
-					reject(err);
-				}else{
-			    	resolve(result);
-				}
-				// in express
-			    // res.render('wxpay/jsapi', { payargs:result })
-			});
-		});
-	}
-};*/
 
 function WXPay() {
 	
@@ -92,7 +61,7 @@ WXPay.mix('createWCPayOrder', function(order){
 	order.nonce_str = order.nonce_str || util.generateNonceString();
 	util.mix(order, this.wxpayID);
 	order.sign = this.sign(order);
-	
+	console.log('order:'+JSON.stringify(order))
 	return new Promise((resolve,reject) => {
 		this.requestUnifiedOrder(order,function(err,data){
 			if (err) {
