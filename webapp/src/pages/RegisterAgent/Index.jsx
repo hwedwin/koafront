f5365994aef0d6efd1cbdd5fa49178d8901b33f9
MANSOUL <xiaohu12347@gmail.com>
2017-10-14 17:59:20 +0800

    bug fixed

--- webapp/src/pages/RegisterAgent/Index.jsx
+++ webapp/src/pages/RegisterAgent/Index.jsx
@@ -55,11 +55,68 @@ class RegisterAgent extends Component {
 				.then((res) => {
 					if (res.status === 200) {
 						console.log(res);
+						this._handleWXShare(res.data);
 					}else{
 					}
 				}).catch(function(error){
 					console.log(error);
 				});
+	}
+
+	_handleWXShare(data) {
+		var wx = window.wx;
+		wx.config({
+	          debug: true, // 开启调试模式,调用的所有api的返回值会在客户端alert出来，若要查看传入的参数，可以在pc端打开，参数信息会通过log打出，仅在pc端时才会打印。
+	          appId: 'wxd49fa3df1b475fa7', // 必填，公众号的唯一标识
+	          timestamp: data.timestamp, // 必填，生成签名的时间戳
+	          nonceStr: data.nonceStr, // 必填，生成签名的随机串
+	          signature: data.signature,// 必填，签名，见附录1
+	          jsApiList: [
+	          	'onMenuShareTimeline',
+	          	'onMenuShareAppMessage'
+	          ] // 必填，需要使用的JS接口列表，所有JS接口列表见附录2
+	      });
+
+		var title = data.nickname+'邀请您加入麦智合伙人';
+		var link = window.location.href;
+		var logo = 'http://jiuji-test.gz.bcebos.com/logo_100.png';
+		var desc = '邀请您加入麦智合伙人，分享高性价比糖酒食品';
+
+      wx.ready(function(){
+	      	wx.onMenuShareTimeline({
+			    title: title, // 分享标题
+			    link: link, // 分享链接，该链接域名或路径必须与当前页面对应的公众号JS安全域名一致
+			    imgUrl: logo, // 分享图标
+			    success: function () { 
+			        // 用户确认分享后执行的回调函数
+			        Toast.info('已成功分享您的邀请链接');
+			    },
+			    cancel: function () { 
+			        // 用户取消分享后执行的回调函数
+			    }
+			});
+
+			wx.onMenuShareAppMessage({
+			    title: title, // 分享标题
+			    desc: desc, // 分享描述
+			    link: link, // 分享链接，该链接域名或路径必须与当前页面对应的公众号JS安全域名一致
+			    imgUrl: logo, // 分享图标
+			    type: 'link', // 分享类型,music、video或link，不填默认为link
+			    dataUrl: '', // 如果type是music或video，则要提供数据链接，默认为空
+			    success: function () { 
+			        // 用户确认分享后执行的回调函数
+			        Toast.info('已成功分享您的邀请链接');
+			    },
+			    cancel: function () { 
+			        // 用户取消分享后执行的回调函数
+			    }
+			});
+      });
+
+      wx.error(function(res){
+      	console.log('err');
+      	console.log(res);
+      });
 	}
 
 	_handleShareUrl() {
@@ -70,6 +127,7 @@ class RegisterAgent extends Component {
 					if (res.status === 200) {
 						if (res.data.code === 200) {
 							this.props.history.replace('/regagent?aid='+res.data.data.id);
+							this._requestWXJsConfig(res.data.data);
 						}else{
 						}
 					}else{
