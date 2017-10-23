603f25dfd16d0c07134082f08c6a535a4eb87664
MANSOUL <xiaohu12347@gmail.com>
2017-10-24 01:31:43 +0800

    bug fixed

--- routes/index.js
+++ routes/index.js
@@ -2,6 +2,8 @@ const router = require('koa-router')();
 const IndexController = require('../controllers/IndexController')
 
 router.get('/',IndexController.index)
+		.get('/home',IndexController.index)
+		.get('/pay',IndexController.index)
 		.get('/regagent',IndexController.regagent)
 		.get('/wxoauth',IndexController.wxOauth)
 		.post('/api/beat',IndexController.beat)
