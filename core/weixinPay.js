414b77d8f43303e876da6c5405cc3dd16123fef0
MANSOUL <xiaohu12347@gmail.com>
2017-10-29 16:14:25 +0800

    debug

--- core/weixinPay.js
+++ core/weixinPay.js
@@ -13,7 +13,6 @@ function WXPay() {
 	};
 
 	this.options = arguments[0];
-	this.options.pfx = fs.readFileSync(path.resolve(__dirname,'../cert/apiclient_cert.p12'));
 	this.wxpayID = { appid:wxConfig.appid, mch_id:wxConfig.mch_id };
 };
 
@@ -120,8 +119,6 @@ WXPay.mix('requestUnifiedOrder',function(order,fn,errFn){
  * spbill_create_ip
  */
 WXPay.mix('createTransferOrder', function(order){
-	console.log(1);
-	console.log(order);
 	order.spbill_create_ip = order.spbill_create_ip.match(/\d+.\d+.\d+.\d+/)[0];
 	order.trade_type = "JSAPI";
 	order.nonce_str = order.nonce_str || util.generateNonceString();
@@ -132,8 +129,6 @@ WXPay.mix('createTransferOrder', function(order){
 	order.desc = '用户提现';
 	order.sign = this.sign(order);
 	var self = this;
-	console.log(2);
-	console.log(order);
 	return new Promise(function(resolve,reject) {
 		self.requestTransferOrder(order,function(err,data) {
 			if (err) {
@@ -154,7 +149,7 @@ WXPay.mix('requestTransferOrder',function(order,fn,errFn){
 		method: 'POST',
 		body: util.buildXML(order),
 		agentOptions: {
-			pfx: this.options.pfx,
+			pfx: fs.readFileSync(path.resolve(__dirname,'../cert/apiclient_cert.p12')),
 			passphrase: wxConfig.mch_id
 		}
 	}, function(err, response, body){
