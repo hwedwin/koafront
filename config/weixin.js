2087bec125338cf3306b85b5c098b03cb6d78383
MANSOUL <xiaohu12347@gmail.com>
2017-11-11 18:35:18 +0800

    修改微信

--- config/weixin.js
+++ config/weixin.js
@@ -1,6 +1,17 @@
-module.exports = {
+/*module.exports = {
 	appid: 'wxd49fa3df1b475fa7',//微信支付分配的公众账号ID
 	mch_id: '1486644772',//微信支付分配的商户号
 	appSecret: '2fa0bf30a63d3c1bf3e71737dfdce311',
 	mch_key: 'ahgy785fvgbc123dfvalo08hndbg5fva'
+};*/
+
+
+/*
+正式商城配置
+ */
+module.exports = {
+	appid: 'wx401d956d445ec5aa',//微信支付分配的公众账号ID
+	mch_id: '1491984782',//微信支付分配的商户号
+	appSecret: '95c946ce97e2f21db1ab6780eea403e5',
+	mch_key: 'ahgy123fvbbc089dfvalo08hnnbg5fha'
 };
