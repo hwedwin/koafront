d286a6b3c86c3309a17557990dc9060f70e4474b
MANSOUL <xiaohu12347@gmail.com>
2017-11-11 23:13:51 +0800

    debug

--- controllers/IndexController.js
+++ controllers/IndexController.js
@@ -1,7 +1,7 @@
 const IndexController = {
     index: async function(ctx) {
         console.log(ctx.session.headerimgurl);
-        ctx.session.headerimgurl = null;
+        ctx.session.openid = null;
         var {aid} = ctx.request.query;
         aid = aid ? aid : '';
         // 进行微信授权
