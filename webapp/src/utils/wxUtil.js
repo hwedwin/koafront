0bb675e6f7b39b70aa979a2f246ed5895d9063d7
MANSOUL <xiaohu12347@gmail.com>
2017-11-12 00:24:33 +0800

    debug

--- webapp/src/utils/wxUtil.js
+++ webapp/src/utils/wxUtil.js
@@ -9,7 +9,7 @@ var wxUtil = {
         var wx = window.wx;
         wx.config({
             debug: false, // 开启调试模式,调用的所有api的返回值会在客户端alert出来，若要查看传入的参数，可以在pc端打开，参数信息会通过log打出，仅在pc端时才会打印。
-            appId: 'wxd49fa3df1b475fa7', // 必填，公众号的唯一标识
+            appId: 'wx401d956d445ec5aa', // 必填，公众号的唯一标识
             timestamp: data.timestamp, // 必填，生成签名的时间戳
             nonceStr: data.nonceStr, // 必填，生成签名的随机串
             signature: data.signature, // 必填，签名，见附录1
